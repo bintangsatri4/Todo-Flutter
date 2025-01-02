309d46456c255a08c732528bd297c156f27f77b3
Bintang Satria Mulya <bintangsatriamulya88@gmail.com>
2025-01-02 18:40:35 +0800

    fix : Fix Bug Home Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/utils/extensions.dart';
+import 'package:gap/gap.dart';
+import 'package:todo_app/widgets/display_white_text.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final colors = context.colorScheme;
+    final deviceSize = context.deviceSize;
     return Scaffold(
       body: Stack(
         children: [
           Column(
             children: [
               Container(
-                height: 300,
-                width: 200,
-                color: Theme.of(context).colorScheme.primary,
+                height: deviceSize.height * 0.3,
+                width: deviceSize.width,
+                color: colors.primary,
+                child: const Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      DisplayWhiteText(text: "May 4, 2025", size: 25,fontWeight: FontWeight.normal,),
+                  Gap(10),
+                  DisplayWhiteText(text: "My Todo", size: 50,fontWeight: FontWeight.bold,),
+
+                    ],
+                  ),
+                ),
                 
               )
             ],
